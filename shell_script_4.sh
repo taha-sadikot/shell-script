a916ae6c4116b75e737cc0c5253dfdf7b2aaf00b
Taha Sadikot <taha.sadikot@weboniselab.com>
2013-07-01 13:37:19 +0530

    fourth assignment

---
+++ shell_script_4.sh
@@ -0,0 +1,5 @@
+echo "Changing mode of file to 777:" >> assignment4.log
+mkdir public_html >> assignment4.log
+touch public_html/readme.html >> assignment4.log
+chmod 777 public_html/readme.html >> assignment4.log
+echo " " >> assignment4.log
